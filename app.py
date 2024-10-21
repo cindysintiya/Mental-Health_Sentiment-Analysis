efb4ddb8eda718b1d41a554a29b0fd12d1d616c5
Cindy Sintiya <211110347@students.mikroskil.ac.id>
2024-10-21 11:45:50 +0700

    update model and design
    - (update) model layers and other parameters
    - (remove) imbalanced labels' data and left with 3 classes only
    - (update) export new model with new eval accuracy 80,...%
    - (update) home page design
    - (remove) vercel deployment

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
 from website import create_app
-
-# Defined model class
-import torch.nn as nn
-
-class SentimentAnalysisModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        logits = self.embd(x)
-        logits , (h_n, c_n) = self.lstm(logits)
-        logits = logits.flatten(start_dim = 1, end_dim=-1)
-        logits = self.linear(logits)
-        
-        softmax = nn.Softmax(dim=1)
-        probability = softmax(logits)
-        
-        return logits, probability
+from data.model import SentimentAnalysisModel
 
 if __name__ == "__main__" :
     app = create_app()
-    # app.run(debug=True)   # auto reload jk file berubah, tdk bs krn auto ada pycache
-    app.run(debug=False)
+    app.run(debug=True)   # auto reload jk file berubah, tdk bs krn auto ada pycache        
+    # app.run(debug=False)
