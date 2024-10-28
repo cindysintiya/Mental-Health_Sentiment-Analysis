e4d383a3daf033bda0f9affe3afb88f8e01e74a2
Cindy Sintiya <211110347@students.mikroskil.ac.id>
2024-10-28 22:06:09 +0700

    better model accuracy n fix app running on debug model
    - (update) model accuracy form 80,38% to 80,50%
    - (fixed) app running on debug mode to false
    - (upload) project report n documentation

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from data.model import SentimentAnalysisModel
 
 if __name__ == "__main__" :
     app = create_app()
-    app.run(debug=True)   # auto reload jk file berubah, tdk bs krn auto ada pycache        
-    # app.run(debug=False)
+    # app.run(debug=True)   # auto reload jk file berubah, tdk bs krn auto ada pycache        
+    app.run(debug=False)
